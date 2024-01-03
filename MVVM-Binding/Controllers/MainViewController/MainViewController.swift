adbeecadf01f27ab2e8325984e80fa696f45e64e
Ibrahim Nasser <thenasser@gmail.com>
2024-01-03 21:45:32 +0200

    Deleting SceneDelegate & main.storyboard

---
+++ MVVM-Binding/Controllers/MainViewController/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  MVVM-Binding
+//
+//  Created by Ibrahim Nasser Ibrahim on 03/01/2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+      self.title = "Main View"
+    }
+
+}
